70b7824b5df09d9c14596de17c3f6e02a35c284a
asad khan <auzair535@gmail.com>
2018-05-04 21:12:34 +0500

    AI-Voice

---
+++ voicerecognizer/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace voicerecognizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(283, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "-log-";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(0, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "disable voice controll";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Location = new System.Drawing.Point(0, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(283, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "enable voice controll";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(282, 268);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ voicerecognizer/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+namespace voicerecognizer
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synt = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            button1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices command = new Choices();
+            command.Add(new string[] { "say hello", "print my name", "open calender", "speak selector text", "open notepad", "close notepad", 
+                "open google chrome","open camera","what date is today","navigate to google","facebook","youtube",
+           "on which os you are running"," file explorer","close camera","close file explorer","close youtube","close facebook" });
+            GrammarBuilder gbuilder = new GrammarBuilder();
+            gbuilder.Append(command);
+            Grammar grammer = new Grammar(gbuilder);
+            recEngine.LoadGrammarAsync(grammer);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+        }
+
+      
+
+       void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text) {
+                case "say hello":
+                    synt.SpeakAsync("hello how are you ");
+                    break;
+                case "print my name":
+                    richTextBox1.Text += "\nshafay";
+                    break;
+
+                case "Open calender":
+                    synt.SpeakAsync("opening calender");
+                    Process.Start("calender.exe");
+                    break;
+
+                case "open notepad":
+                   
+                    Process.Start("notepad.exe");
+                    break;
+
+                case "speak selector text":
+                    synt.SpeakAsync(richTextBox1.Text);
+                    break;
+
+                case "open google chrome":
+                    
+                    Process.Start("chrome.exe");
+                    break;
+
+                case "open camera":
+                    Process process = new Process();
+                    process.StartInfo.FileName = "microsoft.windows.camera:";
+                    process.Start();
+                    break;
+
+           
+                case "what date is today":
+                    synt.SpeakAsync(DateTime.Now.ToString("MM-dd-yyyy"));
+                    break;
+
+                case "facebook":
+                    Process.Start("chrome.exe","https://www.facebook.com");
+              
+                    break;
+
+                case "navigate to google":
+                    Process.Start("chrome.exe","https://www.google.co.in");
+                    break;
+                
+                case "youtube":
+                    Process.Start("chrome.exe","https://www.youtube.com");
+                    break;
+
+                case "on which os you are running":
+                    synt.SpeakAsync("microsoft's windows operating system");
+                    break;
+
+                case "file explorer":
+                    Process.Start("explorer.exe");
+                    break;
+            }
+
+            Process[] pname2 = Process.GetProcessesByName("notepad");
+            if (pname2.Length != 0)
+                switch (e.Result.Text)
+                {
+                    case "close notepad":
+                        pname2[0].Kill();
+                        break;
+                    
+                }
+
+            Process[] pname1 = Process.GetProcessesByName("windowscamera");
+            if (pname1.Length != 0)
+                switch (e.Result.Text)
+                {
+                    case "close camera":
+                        pname1[0].Kill();
+                        break;
+                   
+                }
+
+                Process[] pname3 = Process.GetProcessesByName("explorer");
+                if (pname3.Length != 0)
+                    switch (e.Result.Text)
+                    {
+                        case "close file explorer":
+                            SendKeys.Send("%{f4}");
+                            break;
+                      
+                    }
+
+                Process[] pname4 = Process.GetProcessesByName("tube");
+                if (pname3.Length != 0)
+                    switch (e.Result.Text)
+                    {
+                        case "close youtube":
+                            SendKeys.Send("%{f4}");
+                            break;
+
+                    }
+
+                Process[] pname4 = Process.GetProcessesByName("book");
+                if (pname3.Length != 0)
+                    switch (e.Result.Text)
+                    {
+                        case "close facebook":
+                            SendKeys.Send("%{f4}");
+                            break;
+
+                    }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            button1.Enabled = false;
+        }
+
+       
+    }
+}
